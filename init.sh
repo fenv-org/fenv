1512038afc20980215d55eadcf6e311ee0a4c811
Heejoon Kang <haseo.hackers@gmail.com>
2023-03-01 11:22:10 +0900

    Clones the remote fenv repository with the latest tag

--- init.sh
+++ init.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env sh
 
 download_destination=$(mktemp -d)
-git clone https://github.com/powdream/fenv "$download_destination"
-pushd "$download_destination"> /dev/null
+latest_version="$(git ls-remote --tags https://github.com/powdream/fenv \
+                  | grep 'refs/tags/v[0-9]\+.[0-9]\+.[0-9]\+$' \
+                  | sed "s/^.*tags\///g" \
+                  | sort --version-sort \
+                  | tail -n 1)"
+git clone \
+  -c advice.detachedHead=false \
+  -b "$latest_version" \
+  https://github.com/powdream/fenv \
+  "$download_destination"
+pushd "$download_destination" > /dev/null
 ./setup_fenv.sh
 popd > /dev/null
+rm -rf "$download_destination" > /dev/null
