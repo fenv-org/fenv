fcc3bcde8d91b79473f0b65a230918cbb84a30bc
Heejoon Kang <haseo.hackers@gmail.com>
2023-04-14 08:28:49 +0900

    first rust args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[clap(name = "fenv", author, about, version)]
+struct FenvArgs {
+    /// Turn debugging information on
+    #[arg(short, long, action = clap::ArgAction::SetTrue)]
+    debug: bool,
+
+    #[command(subcommand)]
+    command: FenvSubcommands,
+}
+
+#[derive(Debug, Subcommand)]
+enum FenvSubcommands {
+    /// A command that helps registering `fenv` to your `PATH` env. variable
+    Init(FenvInitArgs),
+}
+
+#[derive(Debug, clap::Args)]
+struct FenvInitArgs {
+    /// Detects the current running shell
+    #[arg(long = "detect-shell", action = clap::ArgAction::SetTrue)]
+    detect_shell: bool,
+
+    /// `-` shows shell instructions to add `fenv` to the `PATH`
+    path_mode: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = FenvArgs::parse();
+    print!("{:?}", args);
 }
