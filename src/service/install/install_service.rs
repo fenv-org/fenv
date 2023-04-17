603f26176e9a9595fc86fc7cb481882544929297
Heejoon Kang <haseo.hackers@gmail.com>
2023-04-17 11:18:51 +0900

    tweak a bit

--- src/service/install/install_service.rs
+++ src/service/install/install_service.rs
@@ -19,87 +19,14 @@ impl FenvInstallService {
     }
 
     pub fn list_remote_sdks() -> Result<Vec<RemoteFlutterSdk>> {
-        let mut sdks_by_tags = FenvInstallService::list_remote_sdks_by_tags()?;
-        let mut sdks_by_refs = FenvInstallService::list_remote_sdks_by_branches()?;
-        let mut merged: Vec<RemoteFlutterSdk> = Vec::new();
-        merged.append(&mut sdks_by_tags);
-        merged.append(&mut sdks_by_refs);
-        Ok(merged)
-    }
-
-    fn list_remote_sdks_by_tags() -> Result<Vec<RemoteFlutterSdk>> {
-        const ERROR_MESSAGE: &str =
-            "Failed to fetch remote tags from `https://github.com/flutter/flutter.git`";
-
-        let command_output = Command::new("git")
-            .arg("ls-remote")
-            .arg("--tags")
-            .arg("https://github.com/flutter/flutter.git")
-            .output()
-            .context(ERROR_MESSAGE)?;
-        if !command_output.status.success() {
-            debug!(
-                "list_remote_sdks_by_tags(): stderr:\n{}",
-                String::from_utf8(command_output.stderr)?
-            );
-            bail!("{}: {}", ERROR_MESSAGE, command_output.status);
-        }
-        let git_output = String::from_utf8(command_output.stdout)?;
-        debug!("list_remote_sdks_by_tags(): stdout:\n{git_output}");
-        let mut lines = git_output.split("\n");
-        // Holds kind keys for eliminating duplications
-        let mut registered_kind_keys: HashSet<String> = HashSet::new();
-        let mut git_refs = lines
-            .by_ref()
-            .map(|line| RemoteFlutterSdk::parse(line))
-            .flatten()
-            // Remove duplications
-            .filter(|sdk| {
-                let key = sdk.kind.key();
-                if registered_kind_keys.contains(&key) {
-                    false
-                } else {
-                    registered_kind_keys.insert(key);
-                    true
-                }
-            })
-            .collect::<Vec<RemoteFlutterSdk>>();
-        git_refs.sort_by(|a, b| a.kind.cmp(&b.kind));
-        Ok(git_refs)
-    }
-
-    fn list_remote_sdks_by_branches() -> Result<Vec<RemoteFlutterSdk>> {
-        const ERROR_MESSAGE: &str =
-            "Failed to fetch remote branches from `https://github.com/flutter/flutter.git`";
-
-        let command_output = Command::new("git")
-            .arg("ls-remote")
-            .args(["--heads", "--refs"])
-            .arg("https://github.com/flutter/flutter.git")
-            .args(["stable", "beta", "master"])
-            .output()
-            .context(ERROR_MESSAGE)?;
-        if !command_output.status.success() {
-            debug!(
-                "list_remote_sdks_by_branches(): stderr:\n{}",
-                String::from_utf8(command_output.stderr)?
-            );
-            bail!("{}: {}", ERROR_MESSAGE, command_output.status);
-        }
-        let git_output = String::from_utf8(command_output.stdout)?;
-        debug!("list_remote_sdks_by_branches(): stdout:\n{git_output}");
-        let mut lines = git_output.split("\n");
-        let git_refs = lines
-            .by_ref()
-            .map(|line| RemoteFlutterSdk::parse(line))
-            .flatten()
-            .collect::<Vec<RemoteFlutterSdk>>();
-        Ok(git_refs)
+        let mut sdks = list_remote_sdks_by_tags()?;
+        sdks.extend(list_remote_sdks_by_branches()?);
+        Ok(sdks)
     }
 }
 
 impl Service for FenvInstallService {
-    fn execute(&self, config: &Config) -> Result<()> {
+    fn execute(&self, _: &Config) -> Result<()> {
         if self.args.list {
             let sdks = FenvInstallService::list_remote_sdks()?;
             for sdk in sdks {
@@ -127,4 +54,74 @@ impl GitRefsKind {
             GitRefsKind::Head(branch) => String::from(branch),
         }
     }
+}
+
+fn list_remote_sdks_by_tags() -> Result<Vec<RemoteFlutterSdk>> {
+    const ERROR_MESSAGE: &str =
+        "Failed to fetch remote tags from `https://github.com/flutter/flutter.git`";
+
+    let command_output = Command::new("git")
+        .arg("ls-remote")
+        .arg("--tags")
+        .arg("https://github.com/flutter/flutter.git")
+        .output()
+        .context(ERROR_MESSAGE)?;
+    if !command_output.status.success() {
+        debug!(
+            "list_remote_sdks_by_tags(): stderr:\n{}",
+            String::from_utf8(command_output.stderr)?
+        );
+        bail!("{}: {}", ERROR_MESSAGE, command_output.status);
+    }
+    let git_output = String::from_utf8(command_output.stdout)?;
+    debug!("list_remote_sdks_by_tags(): stdout:\n{git_output}");
+    let mut lines = git_output.split("\n");
+    // Holds kind keys for eliminating duplications
+    let mut registered_kind_keys: HashSet<String> = HashSet::new();
+    let mut git_refs = lines
+        .by_ref()
+        .map(|line| RemoteFlutterSdk::parse(line))
+        .flatten()
+        // Remove duplications
+        .filter(|sdk| {
+            let key = sdk.kind.key();
+            if registered_kind_keys.contains(&key) {
+                false
+            } else {
+                registered_kind_keys.insert(key);
+                true
+            }
+        })
+        .collect::<Vec<RemoteFlutterSdk>>();
+    git_refs.sort_by(|a, b| a.kind.cmp(&b.kind));
+    Ok(git_refs)
+}
+
+fn list_remote_sdks_by_branches() -> Result<Vec<RemoteFlutterSdk>> {
+    const ERROR_MESSAGE: &str =
+        "Failed to fetch remote branches from `https://github.com/flutter/flutter.git`";
+
+    let command_output = Command::new("git")
+        .arg("ls-remote")
+        .args(["--heads", "--refs"])
+        .arg("https://github.com/flutter/flutter.git")
+        .args(["stable", "beta", "master"])
+        .output()
+        .context(ERROR_MESSAGE)?;
+    if !command_output.status.success() {
+        debug!(
+            "list_remote_sdks_by_branches(): stderr:\n{}",
+            String::from_utf8(command_output.stderr)?
+        );
+        bail!("{}: {}", ERROR_MESSAGE, command_output.status);
+    }
+    let git_output = String::from_utf8(command_output.stdout)?;
+    debug!("list_remote_sdks_by_branches(): stdout:\n{git_output}");
+    let mut lines = git_output.split("\n");
+    let git_refs = lines
+        .by_ref()
+        .map(|line| RemoteFlutterSdk::parse(line))
+        .flatten()
+        .collect::<Vec<RemoteFlutterSdk>>();
+    Ok(git_refs)
 }
