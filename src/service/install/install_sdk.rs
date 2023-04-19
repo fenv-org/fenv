6095eaa9ca227308ce00db82cd0d8bf181acc6e4
Heejoon Kang <haseo.hackers@gmail.com>
2023-04-19 09:36:59 +0900

    separate functions to manage sdk root and marker files

--- src/service/install/install_sdk.rs
+++ src/service/install/install_sdk.rs
@@ -1,7 +1,7 @@
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 
 use anyhow::{anyhow, bail, Context, Ok, Result};
-use log::debug;
+use log::{debug, info};
 
 use super::{flutter_command::FlutterCommand, git_command::GitCommand};
 
@@ -12,39 +12,34 @@ pub(crate) fn install_sdk(
     git_command: &Box<dyn GitCommand>,
     flutter_command: &Box<dyn FlutterCommand>,
 ) -> Result<()> {
-    let marker_filepath = format!("{versions_directory}/.install_{target_version_or_channel}");
-    let marker = Path::new(&marker_filepath);
-    let destination_path = format!("{versions_directory}/{target_version_or_channel}");
-    let destination = Path::new(&destination_path);
-    // Remove `destination` and its all children if `marker_filepath` exists.
-    if marker.exists() {
-        debug!("install_sdk(): `{marker_filepath}` exists: remove the `{destination_path}`");
-        if destination.exists() {
-            std::fs::remove_dir_all(destination)
-                .map_err(|e| anyhow!(e))
-                .with_context(|| format!("Failed to remove the `{destination_path}`"))?;
-        }
-    } else if destination.exists() {
-        bail!("`{versions_directory}/{target_version_or_channel}` already exists")
-    }
-
-    /// Only if the given `result` is an `Err`,
-    /// removes the `destination` and its every children and
-    /// returns immediately.
     macro_rules! clear_destination_and_early_return_if_err {
         ($result: ident) => {
             if let Err(_e) = $result {
-                if marker.exists() {
-                    std::fs::remove_file(marker).ok();
-                }
-
-                if destination.exists() {
-                    std::fs::remove_dir_all(&destination).ok();
-                }
+                remove_installing_marker(versions_directory, target_version_or_channel).ok();
+                remove_sdk_root(versions_directory, target_version_or_channel).ok();
                 return Err(_e);
             }
         };
     }
+
+    if is_installed(versions_directory, target_version_or_channel) {
+        bail!("`{}` is already installed", target_version_or_channel)
+    }
+
+    if exists_installing_marker(versions_directory, target_version_or_channel) {
+        info!(
+            "install_sdk(): a previous trial to install `{target_version_or_channel}` \
+            ended unsuccessfully: remove the `{destination}`",
+            destination = sdk_root_of(versions_directory, target_version_or_channel)
+                .to_str()
+                .unwrap()
+        );
+        remove_sdk_root(versions_directory, target_version_or_channel)?;
+        remove_installing_marker(versions_directory, target_version_or_channel)?;
+    }
+
+    let marker = installing_marker_of(versions_directory, target_version_or_channel);
+    let destination = sdk_root_of(versions_directory, target_version_or_channel);
 
     if let Some(parent) = destination.parent() {
         if !parent.exists() {
@@ -57,17 +52,13 @@ pub(crate) fn install_sdk(
     }
 
     // create an empty file to mark as installing the specifying SDK version.
-    if !marker.exists() {
-        debug!(
-            "install_sdk(): create an installing marker file parent directory: `{marker_filepath}`"
-        );
-        std::fs::File::create(marker)
-            .map_err(|e| anyhow!(e))
-            .with_context(|| {
-                format!("Failed to create an installing marker file: `{marker_filepath}`")
-            })?;
-    }
+    debug!(
+        "install_sdk(): create an installing marker file parent directory: `{}`",
+        &marker.to_str().unwrap()
+    );
+    create_installing_marker(versions_directory, target_version_or_channel)?;
 
+    // download the dedicated sdk.
     let clone_result = match target_version_or_channel {
         "stable" | "beta" | "dev" | "master" => git_command
             .clone_flutter_sdk_by_channel(target_version_or_channel, destination.to_str().unwrap()),
@@ -76,15 +67,91 @@ pub(crate) fn install_sdk(
     };
     clear_destination_and_early_return_if_err!(clone_result);
 
+    // install the download sdk.
     let flutter_bin_dir = destination.to_str().unwrap();
     let doctor_result = flutter_command.doctor(flutter_bin_dir);
     clear_destination_and_early_return_if_err!(doctor_result);
 
+    // execute `flutter precache` to install cli tools.
     if do_precache {
         let precache_result = flutter_command.precache(flutter_bin_dir);
         clear_destination_and_early_return_if_err!(precache_result);
     }
 
-    std::fs::remove_file(marker).ok();
+    remove_installing_marker(versions_directory, target_version_or_channel).ok();
+    Ok(())
+}
+
+fn installing_marker_of(versions_directory: &str, target_version_or_channel: &str) -> PathBuf {
+    PathBuf::from(format!(
+        "{versions_directory}/.install_{target_version_or_channel}"
+    ))
+}
+
+fn sdk_root_of(versions_directory: &str, target_version_or_channel: &str) -> PathBuf {
+    PathBuf::from(format!("{versions_directory}/{target_version_or_channel}"))
+}
+
+fn exists_installing_marker(versions_directory: &str, target_version_or_channel: &str) -> bool {
+    installing_marker_of(versions_directory, target_version_or_channel).exists()
+}
+
+fn exists_sdk_root(versions_directory: &str, target_version_or_channel: &str) -> bool {
+    sdk_root_of(versions_directory, target_version_or_channel).exists()
+}
+
+fn is_installed(versions_directory: &str, target_version_or_channel: &str) -> bool {
+    !exists_installing_marker(versions_directory, target_version_or_channel)
+        && exists_sdk_root(versions_directory, target_version_or_channel)
+}
+
+fn create_installing_marker(
+    versions_directory: &str,
+    target_version_or_channel: &str,
+) -> Result<()> {
+    let marker = installing_marker_of(versions_directory, target_version_or_channel);
+    if !marker.exists() {
+        std::fs::File::create(&marker)
+            .map_err(|e| anyhow!(e))
+            .with_context(|| {
+                format!(
+                    "Failed to create an installing marker: `{}`",
+                    marker.to_str().unwrap()
+                )
+            })?;
+    }
+    Ok(())
+}
+
+fn remove_installing_marker(
+    versions_directory: &str,
+    target_version_or_channel: &str,
+) -> Result<()> {
+    let marker = installing_marker_of(versions_directory, target_version_or_channel);
+    if marker.exists() {
+        std::fs::remove_file(&marker)
+            .map_err(|e| anyhow!(e))
+            .with_context(|| {
+                format!(
+                    "Failed to remove an installing marker: `{}`",
+                    marker.to_str().unwrap()
+                )
+            })?
+    }
+    Ok(())
+}
+
+fn remove_sdk_root(versions_directory: &str, target_version_or_channel: &str) -> Result<()> {
+    let sdk_root = sdk_root_of(versions_directory, target_version_or_channel);
+    if sdk_root.exists() {
+        std::fs::remove_dir_all(&sdk_root)
+            .map_err(|e| anyhow!(e))
+            .with_context(|| {
+                format!(
+                    "Failed to remove a sdk root: `{}`",
+                    sdk_root.to_str().unwrap()
+                )
+            })?;
+    }
     Ok(())
 }
