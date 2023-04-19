d1c3030e9842301b0e266cb5adbc1251dd0b2882
Heejoon Kang <haseo.hackers@gmail.com>
2023-04-19 22:38:05 +0900

    introduce `FlutterChannel`

---
+++ src/model/flutter_channel.rs
@@ -0,0 +1,59 @@
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum FlutterChannel {
+    Dev,
+    Beta,
+    Master,
+    Stable,
+}
+
+impl FlutterChannel {
+    pub fn channel_name(&self) -> &'static str {
+        match self {
+            FlutterChannel::Dev => "dev",
+            FlutterChannel::Beta => "beta",
+            FlutterChannel::Master => "master",
+            FlutterChannel::Stable => "stable",
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use rand::seq::SliceRandom;
+
+    use super::FlutterChannel;
+
+    #[test]
+    fn ensure_the_correct_order() {
+        let mut all_values = vec![
+            FlutterChannel::Dev,
+            FlutterChannel::Beta,
+            FlutterChannel::Master,
+            FlutterChannel::Stable,
+        ];
+
+        let mut rng = rand::thread_rng();
+        all_values.shuffle(&mut rng);
+        println!("shuffled: {:?}", all_values);
+
+        all_values.sort();
+        println!("sorted: {:?}", all_values);
+        assert_eq!(
+            vec![
+                FlutterChannel::Dev,
+                FlutterChannel::Beta,
+                FlutterChannel::Master,
+                FlutterChannel::Stable,
+            ],
+            all_values
+        );
+    }
+
+    #[test]
+    fn test_channel_names() {
+        assert_eq!(FlutterChannel::Dev.channel_name(), "dev");
+        assert_eq!(FlutterChannel::Beta.channel_name(), "beta");
+        assert_eq!(FlutterChannel::Master.channel_name(), "master");
+        assert_eq!(FlutterChannel::Stable.channel_name(), "stable");
+    }
+}
--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,2 +1,3 @@
 pub mod flutter_version;
 pub mod remote_flutter_sdk;
+pub mod flutter_channel;
