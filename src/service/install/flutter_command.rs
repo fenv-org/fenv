af2a01576461122589056c4409d26f5c0fbd4667
Heejoon Kang <haseo.hackers@gmail.com>
2023-04-25 10:51:33 +0900

    small refactoring

--- src/service/install/flutter_command.rs
+++ src/service/install/flutter_command.rs
@@ -19,39 +19,45 @@ impl FlutterCommandImpl {
 
 impl FlutterCommand for FlutterCommandImpl {
     fn doctor(&self, flutter_sdk_root: &str) -> Result<()> {
-        let mut command = Command::new("bash");
+        let flutter_bin_directory = [flutter_sdk_root, "bin"].join(std::path::MAIN_SEPARATOR_STR);
+        let mut command = Command::new("flutter");
         spawn_and_wait!(
             command
-                .current_dir(flutter_sdk_root)
-                .env("PATH", flutter_sdk_root_merged_env_path(flutter_sdk_root)?)
-                .arg("-c")
-                .arg("bin/flutter doctor --suppress-analytics"),
+                .current_dir(&flutter_bin_directory)
+                .env(
+                    "PATH",
+                    flutter_sdk_root_merged_env_path(&flutter_bin_directory)?
+                )
+                .args(["doctor", "--suppress-analytics", "--verbose"]),
             "doctor",
-            "Failed to execute `flutter doctor` on `{flutter_sdk_root}/bin`",
+            "Failed to execute `flutter doctor` on `{flutter_bin_directory}`",
         );
         Ok(())
     }
 
     fn precache(&self, flutter_sdk_root: &str) -> Result<()> {
-        let mut command = Command::new("bash");
+        let flutter_bin_directory = [flutter_sdk_root, "bin"].join(std::path::MAIN_SEPARATOR_STR);
+        let mut command = Command::new("flutter");
         spawn_and_wait!(
             command
-                .current_dir(flutter_sdk_root)
-                .env("PATH", flutter_sdk_root_merged_env_path(flutter_sdk_root)?)
-                .arg("-c")
-                .arg("bin/flutter precache"),
+                .current_dir(&flutter_bin_directory)
+                .env(
+                    "PATH",
+                    flutter_sdk_root_merged_env_path(&flutter_bin_directory)?
+                )
+                .arg("precache"),
             "doctor",
-            "Failed to execute `flutter precache` on `{flutter_sdk_root}/bin`",
+            "Failed to execute `flutter precache` on `{flutter_bin_directory}`",
         );
         Ok(())
     }
 }
 
 /// Generates a new PATH environment value by merging the given `flutter_sdk_root` with the `PATH` environment.
-fn flutter_sdk_root_merged_env_path(flutter_sdk_root: &str) -> Result<String> {
+fn flutter_sdk_root_merged_env_path(flutter_bin_directory: &str) -> Result<String> {
     let env_path = &env::var("PATH").unwrap_or_default();
     let mut current_env_path = env::split_paths(env_path).collect::<Vec<_>>();
-    current_env_path.insert(0, PathBuf::from(flutter_sdk_root).join("bin"));
+    current_env_path.insert(0, PathBuf::from(flutter_bin_directory));
     env::join_paths(&current_env_path)
         .map(|s| s.to_string_lossy().to_string())
         .map_err(|e| anyhow::anyhow!(e))
