cded84bc18836e414135876717847e2c4c1fb59d
Heejoon Kang <haseo.hackers@gmail.com>
2023-04-30 12:27:18 +0900

    polish a bit

--- src/sdk_service/sdk_service.rs
+++ src/sdk_service/sdk_service.rs
@@ -60,8 +60,7 @@ pub struct ReadVersionFileResult {
     pub installed: bool,
 }
 
-// #[derive(Clone)]
-struct SdkServiceDependencies<G: GitCommand, C: Clock> {
+struct SdkServiceInner<G: GitCommand, C: Clock> {
     git_command: G,
     clock: C,
     local_sdk_repository: LocalSdkRepository,
@@ -69,115 +68,14 @@ struct SdkServiceDependencies<G: GitCommand, C: Clock> {
     remote_sdk_list_cache: RemoteSdkListCache,
 }
 
-impl<G, C> SdkServiceDependencies<G, C>
-where
-    G: GitCommand,
-    C: Clock,
-{
-}
-
-impl<G, C> SdkService for SdkServiceDependencies<G, C>
-where
-    G: GitCommand,
-    C: Clock,
-{
-    fn get_installed_sdk_list(
-        &self,
-        context: &impl FenvContext,
-    ) -> anyhow::Result<Vec<LocalFlutterSdk>> {
-        self.local_sdk_repository.ensure_versions_exists(context)?;
-        self.local_sdk_repository.get_installed_sdk_list(context)
-    }
-
-    fn get_available_remote_sdk_list(
-        &self,
-        context: &impl FenvContext,
-    ) -> anyhow::Result<Vec<RemoteFlutterSdk>> {
-        if let Some(sdks) = self.remote_sdk_list_cache.load_list(context, &self.clock) {
-            debug!("sdk list from cache");
-            return anyhow::Ok(sdks);
-        }
-
-        let result = self
-            .remote_sdk_repository
-            .fetch_available_sdk_list(&self.git_command);
-        if let Ok(sdks) = &result {
-            debug!("sdk list from remote");
-            if let Err(e) = self
-                .remote_sdk_list_cache
-                .store_list(context, &self.clock, sdks)
-            {
-                warn!("{e}");
-            }
-        }
-        result
-    }
-
-    fn is_installed(&self, context: &impl FenvContext, version_or_channel: &str) -> bool {
-        self.local_sdk_repository
-            .is_installed(context, version_or_channel)
-    }
-
-    fn find_nearest_version_file(
-        &self,
-        context: &impl FenvContext,
-        start_dir: &PathLike,
-    ) -> anyhow::Result<PathLike> {
-        self.local_sdk_repository
-            .find_nearest_local_version_file(start_dir)
-            .or_else(|| self.local_sdk_repository.find_global_version_file(context))
-            .context("Could not find any version file")
-    }
-
-    fn read_version_file(
-        &self,
-        context: &impl FenvContext,
-        version_file: &PathLike,
-    ) -> anyhow::Result<ReadVersionFileResult> {
-        self.local_sdk_repository
-            .read_version_file(context, version_file)
-            .map(|(sdk, installed)| ReadVersionFileResult { sdk, installed })
-    }
-
-    fn find_global_version_file(&self, context: &impl FenvContext) -> anyhow::Result<PathLike> {
-        self.local_sdk_repository
-            .find_global_version_file(context)
-            .context("Could not find the global version file")
-    }
-
-    fn find_latest_local(
-        &self,
-        context: &impl FenvContext,
-        prefix: &str,
-    ) -> anyhow::Result<LocalFlutterSdk> {
-        let sdk_or_none = self
-            .local_sdk_repository
-            .find_latest_local(context, prefix)?;
-        match sdk_or_none {
-            Some(sdk) => anyhow::Ok(sdk),
-            None => Result::Err(anyhow::anyhow!(
-                "Not found any matched flutter sdk version: `{prefix}`"
-            )),
-        }
-    }
-
-    fn find_latest_remote(
-        &self,
-        _context: &impl FenvContext,
-        _prefix: &str,
-    ) -> anyhow::Result<RemoteFlutterSdk> {
-        todo!()
-    }
-}
-
 pub struct RealSdkService<G: GitCommand, C: Clock> {
-    dependencies: SdkServiceDependencies<G, C>,
+    inner: SdkServiceInner<G, C>,
 }
 
 impl RealSdkService<GitCommandImpl, SystemClock> {
     pub fn new() -> Self {
         Self {
-            dependencies: SdkServiceDependencies {
+            inner: SdkServiceInner {
                 git_command: GitCommandImpl::new(),
                 clock: SystemClock,
                 local_sdk_repository: LOCAL_SDK_REPOSITORY,
@@ -195,7 +93,7 @@ where
 {
     pub fn from(git_command: G, clock: C) -> Self {
         Self {
-            dependencies: SdkServiceDependencies {
+            inner: SdkServiceInner {
                 git_command,
                 clock,
                 local_sdk_repository: LOCAL_SDK_REPOSITORY,
@@ -203,6 +101,32 @@ where
                 remote_sdk_list_cache: REMOTE_SDK_LIST_CACHE,
             },
         }
+    }
+}
+
+impl<'a, G, C> RealSdkService<G, C>
+where
+    G: GitCommand,
+    C: Clock,
+{
+    fn local(&'a self) -> &'a LocalSdkRepository {
+        &self.inner.local_sdk_repository
+    }
+
+    fn remote(&'a self) -> &'a RemoteSdkRepository {
+        &self.inner.remote_sdk_repository
+    }
+
+    fn remote_list_cache(&'a self) -> &'a RemoteSdkListCache {
+        &self.inner.remote_sdk_list_cache
+    }
+
+    fn git_command(&'a self) -> &'a G {
+        &self.inner.git_command
+    }
+
+    fn clock(&'a self) -> &'a C {
+        &self.inner.clock
     }
 }
 
@@ -215,18 +139,34 @@ where
         &self,
         context: &impl FenvContext,
     ) -> anyhow::Result<Vec<LocalFlutterSdk>> {
-        self.dependencies.get_installed_sdk_list(context)
+        self.local().ensure_versions_exists(context)?;
+        self.local().get_installed_sdk_list(context)
     }
 
     fn get_available_remote_sdk_list(
         &self,
         context: &impl FenvContext,
     ) -> anyhow::Result<Vec<RemoteFlutterSdk>> {
-        self.dependencies.get_available_remote_sdk_list(context)
+        if let Some(sdks) = self.remote_list_cache().load_list(context, self.clock()) {
+            debug!("sdk list from cache");
+            return anyhow::Ok(sdks);
+        }
+
+        let result = self.remote().fetch_available_sdk_list(self.git_command());
+        if let Ok(sdks) = &result {
+            debug!("sdk list from remote");
+            if let Err(e) = self
+                .remote_list_cache()
+                .store_list(context, self.clock(), sdks)
+            {
+                warn!("{e}");
+            }
+        }
+        result
     }
 
     fn is_installed(&self, context: &impl FenvContext, version_or_channel: &str) -> bool {
-        self.dependencies.is_installed(context, version_or_channel)
+        self.local().is_installed(context, version_or_channel)
     }
 
     fn find_nearest_version_file(
@@ -234,8 +174,10 @@ where
         context: &impl FenvContext,
         start_dir: &PathLike,
     ) -> anyhow::Result<PathLike> {
-        self.dependencies
-            .find_nearest_version_file(context, start_dir)
+        self.local()
+            .find_nearest_local_version_file(start_dir)
+            .or_else(|| self.local().find_global_version_file(context))
+            .context("Could not find any version file")
     }
 
     fn read_version_file(
@@ -243,11 +185,15 @@ where
         context: &impl FenvContext,
         version_file: &PathLike,
     ) -> anyhow::Result<ReadVersionFileResult> {
-        self.dependencies.read_version_file(context, version_file)
+        self.local()
+            .read_version_file(context, version_file)
+            .map(|(sdk, installed)| ReadVersionFileResult { sdk, installed })
     }
 
     fn find_global_version_file(&self, context: &impl FenvContext) -> anyhow::Result<PathLike> {
-        self.dependencies.find_global_version_file(context)
+        self.local()
+            .find_global_version_file(context)
+            .context("Could not find the global version file")
     }
 
     fn find_latest_local(
@@ -255,14 +201,20 @@ where
         context: &impl FenvContext,
         prefix: &str,
     ) -> anyhow::Result<LocalFlutterSdk> {
-        self.dependencies.find_latest_local(context, prefix)
+        let sdk_or_none = self.local().find_latest_local(context, prefix)?;
+        match sdk_or_none {
+            Some(sdk) => anyhow::Ok(sdk),
+            None => Result::Err(anyhow::anyhow!(
+                "Not found any matched flutter sdk version: `{prefix}`"
+            )),
+        }
     }
 
     fn find_latest_remote(
         &self,
-        context: &impl FenvContext,
-        prefix: &str,
+        _context: &impl FenvContext,
+        _prefix: &str,
     ) -> anyhow::Result<RemoteFlutterSdk> {
-        self.dependencies.find_latest_remote(context, prefix)
+        todo!()
     }
 }
