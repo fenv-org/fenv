748c243cea348fca5ba57365db44448f5f3c2c54
Heejoon Kang <haseo.hackers@gmail.com>
2023-05-20 10:15:50 +0900

    implement service

--- src/service/uninstall/uninstall_service.rs
+++ src/service/uninstall/uninstall_service.rs
@@ -1,6 +1,11 @@
+use log::debug;
+
 use crate::{
-    args::FenvUninstallArgs, context::FenvContext, sdk_service::sdk_service::SdkService,
-    service::service::Service, util::io::ConsoleOutput,
+    args::FenvUninstallArgs,
+    context::FenvContext,
+    sdk_service::{results::LookupResult, sdk_service::SdkService},
+    service::service::Service,
+    util::io::ConsoleOutput,
 };
 
 pub struct FenvUninstallService {
@@ -24,7 +29,47 @@ where
         sdk_service: &impl SdkService,
         output: &mut dyn ConsoleOutput<OUT, ERR>,
     ) -> anyhow::Result<()> {
-        println!("{:?}", self.args);
+        for prefix in &self.args.prefixes {
+            uninstall_version(context, sdk_service, output, prefix)?
+        }
         Ok(())
+    }
+}
+
+fn uninstall_version<OUT, ERR>(
+    context: &impl FenvContext,
+    sdk_service: &impl SdkService,
+    output: &mut dyn ConsoleOutput<OUT, ERR>,
+    prefix: &str,
+) -> anyhow::Result<()>
+where
+    OUT: std::io::Write,
+    ERR: std::io::Write,
+{
+    debug!("Attempting to uninstall `{}`", prefix);
+    let mut lookup_result = sdk_service.find_latest_local(context, prefix);
+    if let LookupResult::None = lookup_result {
+        writeln!(
+            output.stderr(),
+            "Could not find any installed sdk: `{}`",
+            prefix
+        )?;
+        return anyhow::Ok(());
+    }
+
+    loop {
+        match lookup_result {
+            LookupResult::Found(sdk) => {
+                debug!("Found sdk: `{}`", sdk);
+                let result = sdk_service.uninstall(&sdk);
+                if result.is_err() {
+                    break result;
+                }
+                writeln!(output.stdout(), "{}", sdk)?;
+                lookup_result = sdk_service.find_latest_local(context, prefix)
+            }
+            LookupResult::Err(err) => break Result::Err(anyhow::anyhow!(err)),
+            LookupResult::None => break anyhow::Ok(()),
+        }
     }
 }
