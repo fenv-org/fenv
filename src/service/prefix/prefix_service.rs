9ef015b8ae5f1d80d9154426f76848f788444604
Heejoon Kang <haseo.hackers@gmail.com>
2023-05-23 07:45:52 +0900

    Declare macro to invoke other command

--- src/service/prefix/prefix_service.rs
+++ src/service/prefix/prefix_service.rs
@@ -1,6 +1,10 @@
 use crate::{
-    args::FenvPrefixArgs, context::FenvContext, sdk_service::sdk_service::SdkService,
-    service::service::Service, util::io::ConsoleOutput,
+    args::FenvPrefixArgs,
+    context::FenvContext,
+    sdk_service::sdk_service::SdkService,
+    service::service::Service,
+    try_run,
+    util::io::{BufferedOutput, ConsoleOutput},
 };
 
 pub struct FenvPrefixService {
@@ -24,6 +28,35 @@ where
         sdk_service: &impl SdkService,
         output: &mut dyn ConsoleOutput<OUT, ERR>,
     ) -> anyhow::Result<()> {
+        let version_prefix = match &self.args.prefix {
+            Some(prefix) => prefix.to_owned(),
+            None => retrieve_version_name(context, sdk_service, output)?,
+        };
+        // let version_or_channel =
+
         Ok(())
     }
 }
+
+fn retrieve_version_name<OUT, ERR>(
+    context: &impl FenvContext,
+    sdk_service: &impl SdkService,
+    output: &mut dyn ConsoleOutput<OUT, ERR>,
+) -> anyhow::Result<String>
+where
+    OUT: std::io::Write,
+    ERR: std::io::Write,
+{
+    let mut buffered_output = BufferedOutput::new();
+    try_run(
+        &["fenv", "version-name"],
+        context,
+        sdk_service,
+        &mut buffered_output,
+    )?;
+    write!(output.stderr(), "{}", buffered_output.stderr_to_string())?;
+    let version_name = buffered_output.stdout_to_string().trim_end().to_string();
+    return anyhow::Ok(version_name);
+}
+
+// fn resolve_prefix
--- src/service/version/version_service.rs
+++ src/service/version/version_service.rs
@@ -1,10 +1,6 @@
 use crate::{
-    args::FenvStartDirArgs,
-    context::FenvContext,
-    sdk_service::sdk_service::SdkService,
-    service::service::Service,
-    try_run,
-    util::io::{BufferedOutput, ConsoleOutput},
+    args::FenvStartDirArgs, context::FenvContext, invoke_command,
+    sdk_service::sdk_service::SdkService, service::service::Service, util::io::ConsoleOutput,
 };
 
 pub struct FenvVersionService {
@@ -33,55 +29,11 @@ where
         } else {
             context.fenv_dir().to_string()
         };
-        let version_name = retrieve_version_name(context, sdk_service, output, &dir)?;
-        let version_file = retrieve_version_file(context, sdk_service, output, &dir).unwrap();
+        let version_name = invoke_command!(context, sdk_service, output, "version-name", &dir)?;
+        let version_file = invoke_command!(context, sdk_service, output, "version-file", &dir)?;
         writeln!(output.stdout(), "{version_name} (set by `{version_file}`)")?;
         anyhow::Ok(())
     }
-}
-
-fn retrieve_version_name<OUT, ERR>(
-    context: &impl FenvContext,
-    sdk_service: &impl SdkService,
-    output: &mut dyn ConsoleOutput<OUT, ERR>,
-    dir: &str,
-) -> anyhow::Result<String>
-where
-    OUT: std::io::Write,
-    ERR: std::io::Write,
-{
-    let mut buffered_output = BufferedOutput::new();
-    try_run(
-        &["fenv", "version-name", dir],
-        context,
-        sdk_service,
-        &mut buffered_output,
-    )?;
-    write!(output.stderr(), "{}", buffered_output.stderr_to_string())?;
-    let version_name = buffered_output.stdout_to_string().trim_end().to_string();
-    return anyhow::Ok(version_name);
-}
-
-fn retrieve_version_file<OUT, ERR>(
-    context: &impl FenvContext,
-    sdk_service: &impl SdkService,
-    output: &mut dyn ConsoleOutput<OUT, ERR>,
-    dir: &str,
-) -> anyhow::Result<String>
-where
-    OUT: std::io::Write,
-    ERR: std::io::Write,
-{
-    let mut buffered_output = BufferedOutput::new();
-    try_run(
-        &["fenv", "version-file", dir],
-        context,
-        sdk_service,
-        &mut buffered_output,
-    )?;
-    write!(output.stderr(), "{}", buffered_output.stderr_to_string())?;
-    let version_file = buffered_output.stdout_to_string().trim_end().to_string();
-    return anyhow::Ok(version_file);
 }
 
 #[cfg(test)]
