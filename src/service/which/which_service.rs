a1a7876c222bbf21c41bb7c24f18a408892af248
Heejoon Kang <haseo.hackers@gmail.com>
2023-05-24 22:02:28 +0900

    implement `which` command

--- src/service/which/which_service.rs
+++ src/service/which/which_service.rs
@@ -1,7 +1,13 @@
 use crate::{
-    args::FenvWhichArgs, context::FenvContext, sdk_service::sdk_service::SdkService,
-    service::service::Service, util::io::ConsoleOutput,
+    args::FenvWhichArgs,
+    context::FenvContext,
+    invoke_command,
+    sdk_service::sdk_service::SdkService,
+    service::service::Service,
+    util::{io::ConsoleOutput, path_like::PathLike},
 };
+use anyhow::bail;
+use is_executable::is_executable;
 
 pub struct FenvWhichService {
     pub args: FenvWhichArgs,
@@ -24,6 +30,21 @@ where
         sdk_service: &impl SdkService,
         output: &mut dyn ConsoleOutput<OUT, ERR>,
     ) -> anyhow::Result<()> {
-        anyhow::Ok(())
+        let executable = &self.args.executable;
+        let version_or_channel = invoke_command!(context, sdk_service, output, "version-name")?;
+        let prefix = invoke_command!(context, sdk_service, output, "prefix", &version_or_channel)?;
+        let command_path = PathLike::from(prefix.as_str()).join("bin").join(executable);
+        if is_executable(&command_path) {
+            writeln!(output.stdout(), "{}", command_path)?;
+            return anyhow::Ok(());
+        }
+
+        let command_path = context.pub_cache().join("bin").join(executable);
+        if is_executable(&command_path) {
+            writeln!(output.stdout(), "{}", command_path)?;
+            return anyhow::Ok(());
+        }
+
+        bail!("Could not find the specified executable: `{executable}`")
     }
 }
