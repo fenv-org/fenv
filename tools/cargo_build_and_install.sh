8dbf76b88f2fab3df1bb6d4539d7c98f58acbd8d
Heejoon Kang <haseo.hackers@gmail.com>
2023-05-25 23:38:36 +0900

    install built artifact

--- tools/cargo_build_and_install.sh
+++ tools/cargo_build_and_install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -euox
+
 cargo clean
 cargo build --release --locked
 cargo test --release --locked
@@ -15,5 +17,14 @@ fi
 if ! [[ -d "$fenv_home/bin" ]]; then
   mkdir -p "$fenv_home/bin"
 fi
+if ! [[ -d "$fenv_home/shims" ]]; then
+  mkdir -p "$fenv_home/shims"
+fi
+rm -f $fenv_home/bin/fenv* $fenv_home/shims/*
+
+# copy the build `fenv` to `$fenv_home/bin`
 cp $artifact $fenv_home/bin/fenv
 
+# copy scripts in `shims` to `$fenv_home/shims`
+cp ./shims/* $fenv_home/shims
+chmod +x $fenv_home/shims/*
