873dae47e3091523e57e4ca7e3fb8f3ab3c223b5
Heejoon Kang <haseo.hackers@gmail.com>
2025-06-14 19:47:43 +0900

    refactor: address linter

--- src/service/install/install_service.rs
+++ src/service/install/install_service.rs
@@ -87,7 +87,6 @@ mod tests {
         util::chrono_wrapper::SystemClock,
         write_invalid_utf8,
     };
-    use std::{env::consts, io::Write};
 
     define_mock_valid_git_command!();
     define_mock_flutter_command!();
